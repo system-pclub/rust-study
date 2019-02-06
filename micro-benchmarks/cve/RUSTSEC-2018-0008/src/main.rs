250706be4db1995fefad4b38552130f3e85581a3
Yilun <chen2709@purdue.edu>
2019-02-05 20:44:26 -0800

    reproduced RUSTSEC-2018-0010

--- micro-benchmarks/cve/RUSTSEC-2018-0008/src/main.rs
+++ micro-benchmarks/cve/RUSTSEC-2018-0008/src/main.rs
@@ -4,6 +4,9 @@ use slice_deque::SliceDeque;
 
 /**
  *  This bug need to be reproduced with release build
+ *      - Get slice_deque source code - git clone https://github.com/gnzlbg/slice_deque
+ *      - git checkout 57b1a84
+ *      - compile this package and run
  */
 const VALUE: [i32; 3] = [45, 46, 47];
 
